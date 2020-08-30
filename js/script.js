07d235aa8c5e770212eb8294526a23822eeb9fed
umer33 <umerhayat520@gmail.com>
2020-08-30 17:39:39 +0500

    linking sections

--- js/script.js
+++ js/script.js
@@ -81,3 +81,41 @@ lightBox.addEventListener("click", function (event) {
         toggleLightBox();
     }
 })
+
+
+// Aside Navbar
+
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length,
+    allSection = document.querySelectorAll(".section"),
+    totalSection = allSection.length;
+
+for (let i = 0; i < totalNavList; i++) {
+    const a = navList[i].querySelector("a");
+    a.addEventListener("click", function () {
+        // Remove back section class
+        for (let i = 0; i < totalSection; i++) {
+            allSection[i].classList.remove("back-section");
+        }
+
+        for (let j = 0; j < totalNavList; j++) {
+            if (navList[j].querySelector("a").classList.contains("active")) {
+                // Add Back Section Class
+                allSection[j].classList.add("back-section");
+            }
+            navList[j].querySelector("a").classList.remove("active");
+        }
+        this.classList.add("active");
+        showSection(this);
+    })
+}
+function showSection(element) {
+
+    for (let i = 0; i < totalSection; i++) {
+        allSection[i].classList.remove("active");
+    }
+    const target = element.getAttribute("href").split("#")[1];
+
+    document.querySelector("#" + target).classList.add("active");
+}
