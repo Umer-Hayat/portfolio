dd0775bd8abfcde785d438dc5ea4e18182aa129d
umer33 <umerhayat520@gmail.com>
2020-08-30 18:03:11 +0500

    adding animation

--- js/script.js
+++ js/script.js
@@ -119,3 +119,18 @@ function showSection(element) {
 
     document.querySelector("#" + target).classList.add("active");
 }
+
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+    aside = document.querySelector(".aside");
+
+navTogglerBtn.addEventListener("click", () => {
+    asideSectionTogglerBtn();
+})
+function asideSectionTogglerBtn() {
+
+    aside.classList.toggle("open");
+    navTogglerBtn.classList.toggle("open");
+    for (let i = 0; i < totalSection; i++) {
+        allSection[i].classList.toggle("open");
+    }
+}
